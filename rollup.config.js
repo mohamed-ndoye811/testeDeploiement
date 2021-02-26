4340da31b45c2dc7ad683d59bf276363fafe3819
mohamed-ndoye811 <59741838+mohamed-ndoye811@users.noreply.github.com>
2021-02-26 12:14:21 +0100

    Slight Update

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
-import { scss } from "svelte-preprocess";
 import autoPreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
